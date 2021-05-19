8bb3de9eb91ab071387750c4a59aea6c6354bca3
Ryan885 <ryan.osbaldiston@liston.school.nz>
2021-05-20 11:30:31 +1200

    push

--- 02_yes_no_v1.py
+++ 02_yes_no_v1.py
@@ -17,7 +17,10 @@ def yes_no(question):
         else:
             print("Please answer yes / no")
         # neither
+
 # Main Routine Below
+
+
 show_instructions = yes_no("Have you played the game before? ")
 print("You chose {}".format(show_instructions))
 having_fun = yes_no("Are you having fun")
