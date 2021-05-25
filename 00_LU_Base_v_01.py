74fa69b617f38e60b30005ecc7a13467d9d1d8b0
Ryan885 <ryan.osbaldiston@liston.school.nz>
2021-05-25 13:49:17 +1200

    04 HOW MUCH V2

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -25,6 +25,24 @@ def instructions():
     print()
     return ""
 
+def num_check(question, low, high):
+    error ="Please enter a whole number between 1 and 10\n"
+
+    valid = False
+    while not valid:
+        try:
+            # ask the question
+            response = int(input(question))
+            # if the amount is too low / too high give
+            if low < response <= high:
+                return  response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
 # Main Routine Below
 played_before = yes_no("Have you played the game before? ")
 
@@ -32,3 +50,10 @@ if played_before == "no":
     instructions()
 
 print("Program Continues")
+
+print()
+
+# Ask user how much they want to play with...
+how_much = num_check("How much would you like to play with?", 0, 10)
+
+print("You will be spending ${}".format(how_much))
