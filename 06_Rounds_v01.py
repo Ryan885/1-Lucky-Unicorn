f39d332413aa7c44355aecdddd18907cd7860859
Ryan885 <ryan.osbaldiston@liston.school.nz>
2021-06-02 09:16:32 +1200

    04 HOW MUCH V2

--- 06_Rounds_v01.py
+++ 06_Rounds_v01.py
@@ -1,21 +1,56 @@
+
+
 # set balance for testing purposes
 balance = 5
 
 rounds_played = 0
 
-play_again = input("Press <Enter> to play...")
+play_again = input("Press <Enter> to play...").lower()
 while play_again == "":
 
     # increase # of rounds played
     rounds_played += 1
 
     # Print round number
-    print(rounds_played)
-    balance -= 1
-    print("Balance: ", balance)
     print()
+    print("*** Round #{} ***".format(rounds_played))
 
-    play_again = input ("Press Enter to play again or 'xxx' to quit")
+    chosen_num = random.randint(1,100)
+    # adjust balance
+    # if the random # is between 1 and 5
+    # user gets a unicorn (add $4 to balance)
+    if 1 <= chosen_num <=5:
+        chosen = "unicorn"
+        balance += 4
+
+    # if the random # is between 6 and 36,
+    # user gets a donkey (subtract 1 from balance)
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        balance -= 1
+
+    # The token is either a horse or zebra...
+    # In both cases, subtract 0.50 from the balance
+    else:
+        # if the number is even, set the chosen
+        # item to a horse
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+
+        # otherwise set it to a zebra
+        else:
+            chosen = "zebra"
+        balance -= 0.5
+
+    print("You got a {}. Your balance is ${:.2f}".format(chosen, balance))
+
+    if balance < 1:
+        # If balance is too low, exit the game and output a suitable message
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+    else:
+        play_again = input("Press Enter to play again or 'xxx' to quit")
 
 print()
+print("Final balance {}".format(balance))
 
